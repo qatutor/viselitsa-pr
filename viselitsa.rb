ec171209b0ee32efb4829432c973f344ad475651
Vadim Geroim <vgeroim.qa@gmail.com>
2019-04-28 14:28:51 -0400

    move files to lib directory

--- viselitsa.rb
+++ viselitsa.rb
@@ -1,44 +1,32 @@
 # encoding: utf-8
 #
-# Популярная детская игра
+# Популярная детская игра, Виселица
 # https://ru.wikipedia.org/wiki/Виселица_(игра)
-#
-# Этот код необходим только при использовании русских букв на Windows
-if Gem.win_platform?
-  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
-  Encoding.default_internal = __ENCODING__
 
-  [STDIN, STDOUT].each do |io|
-    io.set_encoding(Encoding.default_external, Encoding.default_internal)
-  end
-end
+require 'unicode_utils/upcase'
 
-# Подключаем библиотеку unicode_utils. Предварительно её надо установить, набрав
-# в консоли
-#
-# gem install unicode_utils
-require "unicode_utils"
+require_relative 'lib/game'
+require_relative 'lib/result_printer'
+require_relative 'lib/word_reader'
 
-require_relative "game"
-require_relative "result_printer"
-require_relative "word_reader"
+# Записываем версию игры в константу VERSION
+VERSION = 'Игра виселица, версия 5. (c) Хороший программист'
 
-puts "Игра виселица. Версия 4. (c) goodprogrammer.ru\n\n"
-sleep 1
-
-printer = ResultPrinter.new
-
+# Создаем экземпляр класса WordReader
 word_reader = WordReader.new
+words_file_name = "#{File.dirname(__FILE__)}/data/words.txt"
+word = word_reader.read_from_file(words_file_name)
 
-words_file_name = File.dirname(__FILE__) + "/data/words.txt"
+# Создаем игру и прописываем ее версию с помощью сеттера version=
+game = Game.new(word)
+game.version = VERSION
 
-# Все изменения логики будут у нас в классе Game. Нам нужно в двух местах
-# преобразовать полученные данные в верхний регистр, используя метод upcase
-# подключенного модуля UnicodeUtils: при генерации массива букв из полученного
-# слова в конструкторе и при получении новой буквы от пользователя.
-game = Game.new(word_reader.read_from_file(words_file_name))
+# Теперь экземпляр ResultPrinter-а нельзя создать без игры
+# Именно поэтому порядо создания методов именно такой
+printer = ResultPrinter.new(game)
 
-while game.status == 0
+# Основной игровой цикл остался прежним
+while game.in_progress?
   printer.print_status(game)
   game.ask_next_letter
 end
